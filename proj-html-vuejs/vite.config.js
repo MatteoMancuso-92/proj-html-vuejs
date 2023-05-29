cca572ce4269bac9ad4f07796d9dee5135d21915
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-05-29 18:03:28 +0200

    re start

--- proj-html-vuejs/vite.config.js
+++ proj-html-vuejs/vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import sass from 'sass';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
-      },
-    },
-  },
-
 })
-
-
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import Header from './components/Header.vue';
+import Navbar from './components/Navbar.vue';
+import Jumbo from './components/Jumbo.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 </script>
-
+ <Header />
+ <Navbar />
+ <Jumbo />
+ <Main />
+ <Footer />
 <template>
- 
+  
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+    @import "bootstrap/scss/bootstrap";
+
 
 </style>
